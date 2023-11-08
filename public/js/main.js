44220dc030467f661bfa3cd672844fd4790d03cb
AGATHE Frédérick <fagathe@meilleurtaux.com>
2023-11-08 01:59:10 +0100

    front(layout): create navbar #25

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+// // Dark mode
+// const setColorTheme = (theme) =>
+//   (document.getElementsByTagName("html")[0].dataset.bsTheme = theme);
+
+// if (
+//   window.matchMedia &&
+//   window.matchMedia("(prefers-color-scheme: dark)").matches
+// ) {
+//   setColorTheme("dark");
+// } else {
+//   setColorTheme("light");
+// }
